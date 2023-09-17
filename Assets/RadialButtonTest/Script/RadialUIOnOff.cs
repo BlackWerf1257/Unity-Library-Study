9172da817a7534246a33f92497c69191bcf27de3
BlackWerf1257 <135800974+BlackWerf1257@users.noreply.github.com>
2023-09-18 00:53:21 +0900

    09/18 rme 리스트로 정리 필요 bool 받아오기 관련 수정필요 오브젝트 정렬 관련 수정하기

---
+++ Assets/RadialButtonTest/Script/RadialUIOnOff.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
+
+public class RadialUIOnOff : MonoBehaviour
+{
+    private InputAction inputMove;
+    InputMaps inputAction;
+
+    private GameObject radialObj;
+    [SerializeField] private GameObject radialObjPrefab;
+    public bool isButtonOn;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputAction = new InputMaps();
+        inputMove =  inputAction.Test.Move;
+        inputMove.Enable();
+    }
+
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        if (context.performed && context.interaction is HoldInteraction)
+        {
+            isButtonOn = true;
+            Open();
+        }
+        if (context.canceled)
+        {
+            if (isButtonOn)
+                Close();
+        }
+    }
+    
+    void Open()
+    {
+        radialObj = Instantiate(radialObjPrefab, this.transform.position, Quaternion.identity);
+        radialObj.transform.SetParent(this.gameObject.transform);
+        radialObj.GetComponent<RadicalMenu>().GetParentScript(this.GetComponent<RadialUIOnOff>());
+    }
+    void Close()
+    {
+        if(isButtonOn)
+            Destroy(radialObj);
+    }
+}
---
+++ Assets/RadialButtonTest/Script/RadicalMenuEntry.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using DG.Tweening;
+
+
+public class RadicalMenuEntry : MonoBehaviour, IPointerEnterHandler,IPointerDownHandler, IPointerExitHandler
+{
+    [SerializeField] private Image logo;
+    [SerializeField] private RectTransform rect;
+    [SerializeField] private TextMeshProUGUI currentWeaponTextObj;
+    [SerializeField] private Image thisObj;
+    [SerializeField] private float fadeTime = 10f;
+
+    [SerializeField] private GameObject parentObj;
+    private RadicalMenu parentScriptObj;
+    private bool isButtonOn;
+    
+    [Header("선택한 버튼의 Index")]
+    private int buttonIndex;
+
+    [Tooltip("아이템의 이름")] 
+    private string itemName;
+
+    
+    private void OnEnable()
+    {
+        parentScriptObj = parentObj.GetComponent<RadicalMenu>();
+    }
+
+    public string SetItemName(string name)
+    {
+        return itemName = name;
+    }
+    void SetIcon(Sprite image)
+    {
+        logo.sprite = image;
+    }
+
+    public void ButtonBool(bool boolVar)
+    {
+        isButtonOn = boolVar;
+    }
+
+    public void SetTextObj(TextMeshProUGUI text)
+    {
+        currentWeaponTextObj = text;
+    }
+
+    
+    public void DoFadeIn(Sprite img)
+    {
+        SetIcon(img);
+        thisObj.DOFade(1.0f, fadeTime).onComplete =
+            delegate
+            {
+                logo.enabled = true;
+                logo.DOFillAmount(1.0f, fadeTime);
+                thisObj.DOFillAmount(1.0f, fadeTime);
+                logo.DOFade(1.0f, fadeTime);
+            };
+    }
+    public void DoFadeOut()
+    {
+        logo.DOFade(0f, fadeTime).onComplete = 
+            delegate
+        {
+            Destroy(this.gameObject);
+        }
+        ;
+    }
+
+
+    public int IndexFunc
+    {
+        get { return buttonIndex; }
+        set { buttonIndex = value; }
+    }
+
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        currentWeaponTextObj.text = "Test"; 
+            //label.text;
+            DoFadeOut();
+            Destroy(parentObj);
+            
+        rect.DOComplete();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        rect.DOComplete();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        currentWeaponTextObj.text = buttonIndex.ToString();
+        parentScriptObj.CurrentIndexUpdate(buttonIndex);
+        Debug.Log("Selected Index from Child Button : " + IndexFunc);
+    }
+}
